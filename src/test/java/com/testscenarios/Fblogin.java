b4b960e4bc89a2b2a0e6387dbc80378b996530e3
“seenu4selenium” <seenu4selenium@gmail.com>
2025-04-29 18:55:31 -0600

    Common Functions added

--- src/test/java/com/testscenarios/Fblogin.java
+++ src/test/java/com/testscenarios/Fblogin.java
@@ -3,8 +3,13 @@ package com.testscenarios;
 import org.testng.annotations.Test;
 
 import com.objectrepository.Locators;
+import com.utilities.CommonFunctions;
 
 import org.testng.annotations.BeforeClass;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,38 +17,17 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
-public class Fblogin {
-	public WebDriver driver;
-	//ClassName objectReferenceName = new ClassName();
-	
-	//Create an Object for Locators class
-	Locators loc = new Locators();
-	
+public class Fblogin extends CommonFunctions {
 
 	@Test
 	public void f() throws Exception {
-		driver.get("https://www.facebook.com/");
-		Thread.sleep(3000);
-		//driver.findElement(loc.fbLogin_Email_Editbox).clear();
-		driver.findElement(loc.fbLogin_Email_Editbox).sendKeys("AnushaTest@gmail.sdgsom");
-		driver.findElement(loc.fbLogin_Password_Editbox).sendKeys("testsdgsdgdsg");
-		driver.findElement(loc.fbLogin_Login_Button).click();
-		Thread.sleep(3000);
-	}
-
-	@BeforeClass
-	public void beforeClass() {
-		driver = new ChromeDriver();
-		//driver = new FirefoxDriver();
-		//driver = new EdgeDriver();
-		driver.manage().window().maximize();
-	}
-
-	//@AfterClass
-	public void afterClass() {
-		// Close the current browser/window
-		//driver.close();
+		chromeBrowserLaunch();
+		getURL("fbURL");
+		sendKeysByAnyLocator(loc.fbLogin_Email_Editbox, "fbUN");
+		sendKeysByAnyLocator(loc.fbLogin_Password_Editbox, "fbPWD");
+		clickByAnyLocator(loc.fbLogin_Login_Button);
 		driver.quit();
+
 	}
 
 }
